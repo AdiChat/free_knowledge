634503085342b058e3dbe338beb767c1c5667796
Aditya Chatterjee <aditianhacker@gmail.com>
2017-04-09 01:49:08 +0530

    Get free content :joy:

--- code/javascript/index.js
+++ code/javascript/index.js
@@ -1,3 +1,55 @@
+window.onload = function() 
+{
+    fill_supported_languages();
+};
+
+// Load the available languages in the dropdown menu
+function fill_supported_languages() 
+{
+  var obj = supported_language;
+    var select = document.getElementById("language");
+
+    for (var i=0; i < obj.lang.length; i++)
+    {
+      var option = document.createElement("option");
+      option.id = obj.lang[i].code;
+      option.value = obj.lang[i].code;
+      option.innerHTML = obj.lang[i].language;
+      select.appendChild(option);
+    }
+}
+
+function find_free_content()
+{
+    httpGetAsync(document.getElementById("search-title").value, document.getElementById('language').value)
+}
+
+function httpGetAsync(query, lang) 
+{
+    url = "https://" + lang + ".wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&converttitles=zh&format=json&exintro=&titles=" + query;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () 
+    {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) 
+        {
+            var result = JSON.parse(xmlHttp.responseText);
+            result = result.query.pages;
+            var pageid = Object.keys(result)[0];
+            console.log(result[pageid]);
+            if (pageid != -1) 
+            {
+                document.getElementById('content').innerHTML = '<h2><u>' + result[pageid].title + '</u></h2>' + result[pageid].extract;
+            }
+            else 
+            {
+                document.getElementById('content').innerHTML = "We could not find any free information. 😢";
+            }
+        }
+    };
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+
 // Handle display of sidebar on small screen
 document.getElementById("header").addEventListener("click", function() 
 {
@@ -11,19 +63,3 @@ document.getElementById("header").addEventListener("click", function()
         sidebar.style.display = "none" // Hide sidebar on click
     }
 });
-
-window.onload = function() 
-{
-    // Load the available languages in the dropdown menu
-    var obj = supported_language;
-    var select = document.getElementById("language");
-
-    for (var i=0; i < obj.lang.length; i++)
-    {
-      var option = document.createElement("option");
-      option.id = obj.lang[i].code;
-      option.value = obj.lang[i].code;
-      option.innerHTML = obj.lang[i].language;
-      select.appendChild(option);
-    }
-};
